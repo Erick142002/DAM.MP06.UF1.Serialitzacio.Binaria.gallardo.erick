912d75bad74ad300d763bbdef0a7a5588afa8c3c
Erick <132074706+Erick142002@users.noreply.github.com>
2023-12-08 18:45:05 +0100

    ConfiguracioFitxers.java

--- ConfiguracioFitxers.java
+++ ConfiguracioFitxers.java
@@ -1,7 +1,7 @@
 package Gallardo.Erick.dam.mp05.uf1.p02.serialitzacio.binaria.model.configuration;
 
 public class ConfiguracioFitxers {
-    private static final String FOLDER = "C:/work2/dam/mp06/uf01/fitxers/";
+    private static final String FOLDER = "C:\Users\erick\OneDrive\Desktop\DAM1\DAM.MP06.UF1.Serialitzacio.Binaria.Gallardo.Erick\src";
     public static final String FITXER_COTXE_INDIVIDUAL = FOLDER + "CotxeIndividual.ser";
     public static final String FITXER_LLISTA_COTXES = FOLDER + "LlistaCotxes.ser";
 }
